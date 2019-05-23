53c6ce40ec087812524c60714734f76e94562595
alenka <alenka134@gmail.com>
2019-05-23 22:49:54 +0300

    deleted String for test changes in GitHub

---
+++ src/com/automatization/training/arrays2/Main1.java
@@ -0,0 +1,11 @@
+package com.automatization.training.arrays2;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 56, 89, 90, 100, 78};
+
+        for (int i = 5; i < numbers.length; i--)
+            System.out.println("Number is: " + numbers[i]);
+    }
+
+}
