78c1685cadc312368b80471095db3784b06b07d5
alenka <alenka134@gmail.com>
2019-05-24 11:40:20 +0300

    deleted String for test changes in GitHub

--- src/com/automatization/training/arrays2/Main1.java
+++ src/com/automatization/training/arrays2/Main1.java
@@ -4,8 +4,16 @@ public class Main1 {
     public static void main(String[] args) {
         int[] numbers = {3, 56, 89, 90, 100, 78};
 
-        for (int i = 5; i < numbers.length; i--)
-            System.out.println("Number is: " + numbers[i]);
+        System.out.println("Original array: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println("\n___________________");
+        System.out.println("\nArray in reverse order: ");
+
+        for (int i = numbers.length - 1; i >= 0; i--)
+            System.out.print(numbers[i] + " ");
     }
 
 }
